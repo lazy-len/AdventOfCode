2ebc27c144bfe28cd5c774d11adaa797d08d77f0
Lennard <lennard.breevaart@hva.nl>
2026-01-03 18:26:58 +0100

    added part 2 of day 1

--- src/main/java/AoC2025/Day1.java
+++ src/main/java/AoC2025/Day1.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class day1 {
+public class Day1 {
 
+    public static int dialHitsZero;
 
     static void main() {
-        
-        // Day 1 part 1 :
+
+        // Part 1 : Calculate total times dial ends on 0
         // Load the rotations from the file into a List
         // Loop through the List and check each rotation
         // - if you have to rotate left (L = subtract) or right (R = add)
@@ -19,47 +20,56 @@ public class day1 {
         //    - subtract / add new dialpostion
         //    - think of something that keeps the new dial position between 0 and 99. 
         //    - count the amount of times the dial ends on 0.
-        
+
         // Good to know:
         // Dial goes from 0 - 99
         // 0 - 1 = 99
         // 99 + 1 = 0
         // Dial starts at 50
-        
-        String filepath = "src/main/resources/2025/day1.txt";
+
+
+        // Part 2 : Calculate total times dial hits 0 regardless if it happens during rotation or ends on it.
+        // Add in rotateLeft and rotateRight
+
+
+        String filepath = "src/main/resources/2025/Day1.txt";
         List<String> dialRotations = readFileWithStreams(filepath);
-        
+
         int dial = 50;
-        int answer = 0;
+        dialHitsZero = 0;
+
+        System.out.println("Dial startposition is " + dial);
+        System.out.println();
 
         for (String rotationStr : dialRotations) {
-            // convert rotation string to integer by removing the first letter -> i.e. R48 = 48.
-            int rotation = Integer.parseInt( rotationStr.substring(1) );
 
-            if ( rotationStr.charAt(0) == 'L' ) {
+            // convert rotation string to integer by removing the first letter -> i.e. R48 = 48.
+            int rotation = Integer.parseInt(rotationStr.substring(1));
+
+            if (rotationStr.charAt(0) == 'L') {
                 // L -> rotate left -> subtract
-                dial = rotateLeft( dial, rotation );
+                dial = rotateLeft(dial, rotation);
             }
 
-            if ( rotationStr.charAt(0) == 'R' ) {
+            if (rotationStr.charAt(0) == 'R') {
                 // R -> rotate right -> add
-                dial = rotateRight( dial, rotation );
+                dial = rotateRight(dial, rotation);
             }
 
             if (dial == 0) {
-                // if the dial hits 0 then you have to count that.
-                answer ++;
+                // count the times the dial ends on 0
+                dialHitsZero++;
             }
 
-            // print progress to check.
-//            System.out.printf("The dial is rotated %s to point at %d \n", rotationStr, dial);
-
+            // Print status
+            System.out.printf("The dial is rotated %s to point at %d \n", rotationStr, dial);
         }
 
-        System.out.println("Dial ends on 0 : " + answer);
+        System.out.println("\nDial ends on 0 : " + dialHitsZero);
     }
 
-    static int rotateLeft(int dial, int rotation){
+
+    static int rotateLeft(int dial, int rotation) {
         // calculate the new dial position
         int newDial = dial - rotation;
 
@@ -68,21 +78,30 @@ public class day1 {
             return newDial;
         }
 
-        // if dial is not between 0 and 99. increase it with 100 and try again!
-        return rotateLeft( newDial + 100, 0 );
+        if (dial == 0 ) {
+            return rotateLeft(newDial + 100, 0);
+        }
+        dialHitsZero ++;
+        return rotateLeft(newDial + 100, 0);
+
+        // if dial is not between 0 and 99 increase it with 100 and try again!
 
     }
 
-    static int rotateRight( int dial, int rotation ) {
+    static int rotateRight(int dial, int rotation) {
         int newDial = dial + rotation;
         if (newDial >= 0 && newDial <= 99) {
             return newDial;
+        }
+        
+        if ( newDial > 100 ) {
+            dialHitsZero ++;
         }
         return rotateRight(newDial - 100, 0);
     }
 
 
-    static List<String> readFileWithScanner(String filepath){
+    static List<String> readFileWithScanner(String filepath) {
         List<String> stringList = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(new File(filepath));
@@ -102,7 +121,7 @@ public class day1 {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-            while ((line = reader.readLine()) != null ) {
+            while ((line = reader.readLine()) != null) {
                 stringList.add(line);
             }
         } catch (IOException e) {
